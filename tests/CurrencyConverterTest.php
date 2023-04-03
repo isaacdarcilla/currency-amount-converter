7e792f77cdf2cdc59ee9ed0171d569151d0a3483
Isaac A <isaacdarcilla@gmail.com>
2023-04-03 20:15:51 +0800

    test(unit): added unit test in fluent and api method

--- tests/CurrencyConverterTest.php
+++ tests/CurrencyConverterTest.php
@@ -1,6 +1,11 @@
 <?php
 
+use Currency\CurrencyConverter\Contracts\Converter;
 use Currency\CurrencyConverter\Currency;
+
+use Illuminate\Testing\Fluent\AssertableJson;
+
+use function Pest\Laravel\get;
 
 it('has same exchange rate link', function () {
     expect(config('currency-amount-converter.exchange_rate'))
@@ -16,3 +21,29 @@ it(
         expect($amount)->toBeFloat();
     }
 );
+
+it('has converter route', function () {
+    get(route('currency.convert'))->assertStatus(200);
+});
+
+it('has converts correct rate', function () {
+    $converter = new Converter();
+
+    $rate = $converter->getRate(1, 'USD')->toString();
+
+    $response = get(
+        route(
+            'currency.convert',
+            ['amount' => 1, 'currency' => 'USD']
+        )
+    );
+
+    expect($response->getOriginalContent()['data']['rate'])
+        ->toEqual($rate)
+        ->and($response)->assertJson(fn(AssertableJson $json) => $json
+            ->has('success')
+            ->has('data.rate')
+            ->where('success', 1)
+            ->where('data.rate', $rate)
+        );
+});
